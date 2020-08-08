88e7ce1477e174ddc9cfe0a354b8b424ac8e0e2a
wmireku21 <wmireku21@gmail.com>
2020-08-07 22:25:19 -0500

    connect to MongoDB

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //Bodyparser Middleware
@@ -16,7 +18,9 @@ mongoose
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+// use routes
 
+app.use('/api/items', items);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
