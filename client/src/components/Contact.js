94ad2227b0e48c5b155737ddbb08217bd6507dd3
wmireku21 <wmireku21@gmail.com>
2020-08-09 21:00:47 -0500

    contact/about pages

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { 
+    Col, 
+    Row, 
+    Button, 
+    Form, 
+    FormGroup, 
+    Label, 
+    Input,
+    Container 
+} 
+from 'reactstrap';
+import '../styles/Contact.css';
 
 class Contact extends Component {
-  render() {
-    return (
-        <div>
-            contacts
-        </div>
+    constructor() {
+        super()
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+    
+    render() {
+        return (
+        
+            <div>
+                <div className="contact-background">
+                    <Container className="contact">
+                    <h1>Contact</h1>
+                    <p> Vanity wishes to collaborate with anyone that is willing to make the night one to remember!
+                    If there are any inquiries or you have any questions, please feel free to contact us by filling out the following:</p>
+                            <Form>
+                                <FormGroup for="email">
+                                    <Label><b>Name</b></Label>
+                                    <Input type="text" name="name" id="contact-name" placeholder="Full Name" required/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label><b>Email</b></Label>
+                                    <Input type="email" name="email" id="contact-email" placeholder="Email" required/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label><b>Message</b></Label>
+                                    <Input type="textarea" name="text" id="contact-message" placeholder="Type your message here" required/>
+                                </FormGroup>
+                                <Button> Send Message</Button>
+                            </Form>
+                    </Container>
+                </div>
+            </div>
     );
   }
 }
---
+++ client/src/components/Services.js
@@ -0,0 +1,40 @@
+const services = [
+    {
+        id: 1,
+        img: "https://img.icons8.com/wired/60/000000/tear-off-calendar.png",
+        title: "Event Planning",
+        text: "Planning to host an event, birthday celebration, social gathering, etc. and don’t know where to begin? Don’t worry, for Vanity Leaders will oversee and handle the planning process for you. We’ll help find you affordable venues, room decor, entertainment and more."
+    }, 
+    {
+        id: 2,
+        img: "https://img.icons8.com/dotty/60/000000/marketing.png",
+        title: "Marketing",
+        text: " Vanity Leaders has a growing network system in the city of Chicago, IL and has what it takes to help put you on the map. Allow Vanity Leaders to promote for you or through your business, in order to help further appeal to a larger audience range that can help benefit you both short-term and in the long run."
+    }, 
+    {
+        id: 3,
+        img: "https://img.icons8.com/wired/60/000000/the-toast.png",
+        title: "Entertainment",
+        text: "Vanity Leaders believe in the power of positivity and good vibes. For this very reason, Vanity Leaders hosts our very own social events, musical showcases/open mics, and pop-ups around the city to provide a platform for the talented entertainers in the city to display their talents in a fun and safe space."
+    },
+    {
+        id: 4,
+        img: "https://img.icons8.com/wired/60/000000/art-prices.png",
+        title: "Graphic Design",
+        text: "Do you need help getting people to take notice of your content or do you need cover art for an upcoming project? Tell our team the concept that you have in mind and allow us to create the visual for you that will inform and  captivate consumers."
+    },
+    {
+        id: 5,
+        img: "https://img.icons8.com/wired/60/000000/compact-camera.png",
+        title: "Photography",
+        text: `Along with our own photography services, we are also partnered with Kwabena Kumi Photography, a professional free-lance photographer. Check out his Instagram ${<a href="https://www.instagram.com/kwabenakumiphoto/?igshid=1sbv8bx27akmh">@KwabenaKumiPhotography</a>}.`
+    },
+    {
+        id: 6,
+        img: "https://img.icons8.com/dotty/60/000000/web-design.png",
+        title: "Web Development",
+        text: "Our Web Development team will create you an original site from the ground up through coding, and provide you with a reliable active site that you can show off to the world."
+    }
+]
+
+export default services;
